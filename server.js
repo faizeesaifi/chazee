b32fb195f12fd4956977bd391a6a0cd74c2456db
mohd  faizan <158583647+faizeesaifi@users.noreply.github.com>
2025-05-03 13:15:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 app.use(express.static('public'));
 
 const server = app.listen(process.env.PORT || 3000);
-const wss = new WebSocket.Server({ server });
+const ws = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
+ws.on('connection', (ws) => {
   console.log('Client connected');
   ws.send('Welcome to the WebSocket server!');
   ws.on('message', (message) => {
     console.log('Received:', message.toString());
-    wss.clients.forEach((client) => {
+    ws.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(`Message: ${message}`);
       }
